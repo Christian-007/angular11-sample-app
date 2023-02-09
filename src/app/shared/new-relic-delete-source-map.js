2a2a7f705fa3d45de90625f1511cd6117845f549
Christian Ing Sunardi <christian.sunardi@amarbank.co.id>
2023-02-09 09:56:09 +0700

    feat: make delete-source-map dynamic (delete from listed source maps)

--- src/app/shared/new-relic-delete-source-map.js
+++ src/app/shared/new-relic-delete-source-map.js
@@ -1,14 +1,34 @@
 var deleteSourcemap = require("@newrelic/publish-sourcemap").deleteSourcemap;
-const sourceMapIds = [
-  "2f988525-1c6c-4dbe-b2aa-c2b98720115f",
-  "2fd40601-00f9-4d5f-9c43-3aa020449515",
-  "4cce379f-4c79-4a99-a692-d1edc93b7a8f",
-  "7f9a1a8b-a9e7-4aca-b416-74dd20a15946",
-  "878fb8f8-b651-4d26-8218-2e8ed74d0f60",
-  "8952fee9-807d-4503-a0e7-5df6ae2d8f80",
-  "a34c32eb-c7e8-461b-bbb8-830b91fa337a",
-  "d21f5447-2d5a-4db1-978f-c611a9faeb79",
-];
+var listSourcemaps = require("@newrelic/publish-sourcemap").listSourcemaps;
+
+listSourcemaps(
+  {
+    applicationId: 1385909203,
+    apiKey: "some api key",
+  },
+  function (err, res) {
+    if (err) {
+      console.log("Listing source maps error: ", err);
+      return;
+    }
+
+    const { sourcemaps } = res;
+    console.log("sourcemaps: ", sourcemaps);
+
+    console.log("Deleting all sourcemaps...");
+    const batchDeleteSourceMaps = sourcemaps.map((sourcemap) =>
+      remove(sourcemap.id)
+    );
+    Promise.all(batchDeleteSourceMaps)
+      .then(() => {
+        console.log("Deleting source maps done!");
+      })
+      .catch((err) => {
+        console.warn(`❌ Error deleting sourcemaps: ${err}"`);
+      });
+  }
+);
+
 const remove = (sourcemapId) => {
   return new Promise((resolve, reject) => {
     deleteSourcemap(
@@ -28,12 +48,3 @@ const remove = (sourcemapId) => {
     );
   });
 };
-const batchDeleteSourceMaps = sourceMapIds.map((id) => remove(id));
-
-Promise.all(batchDeleteSourceMaps)
-  .then(() => {
-    console.log("Deleting source maps done!");
-  })
-  .catch((err) => {
-    console.warn(`❌ Error deleting sourcemaps: ${err}"`);
-  });
